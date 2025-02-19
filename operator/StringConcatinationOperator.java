cea77503b364c8f10dbe3761902e686ce359087b
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-02-19 23:49:11 +0530

    Add files via upload

---
+++ operator/StringConcatinationOperator.java
@@ -0,0 +1,20 @@
+package com.operator;
+
+public class StringConcatinationOperator {
+
+	public static void main(String[] args) {
+		
+		///Concatenation operator...  symbol (+)
+		String fName = "Karuna";
+		System.out.println("My name is: "+fName);
+		
+		String lName = " Mishra";
+		
+		String fullName = fName + lName;
+		System.out.println("Full Name: " + fullName);
+		
+
+
+	}
+
+}
