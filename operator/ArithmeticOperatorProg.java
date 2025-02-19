09536ec420a50325dc2dd312fde73d6efc5f6d7d
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-02-19 23:47:25 +0530

    Add files via upload

---
+++ operator/ArithmeticOperatorProg.java
@@ -0,0 +1,44 @@
+package com.operator;
+
+public class ArithmeticOperatorProg {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 20;
+		
+		int result=0;
+		
+		///Displaying the value of variable...
+		System.out.println("Var1: "+var1);
+		System.out.println("Var2: "+var2);
+		System.out.println("Result: "+result);
+
+		//Addition operator symbol (+)
+//		
+//		result = var1 + var2;
+//		System.out.println("After sum Result: "+result);
+		
+//		///Subtraction operator symbol (-)
+//		
+//		result = var1 - var2;
+//		System.out.println("After subtracting Result: "+result);
+		
+//		///Multiplication operator symbol (*)
+//		
+//		result = var1 * var2;
+//		System.out.println("After multiplication Result: "+result);
+		
+		///Division operator symbol (/)
+		
+//		result = var1 / var2;
+//		System.out.println("After division Result: "+result);
+
+		///Remainder operator symbol (%)
+		result = var1 % var2;
+		System.out.println("After division Result: "+result);
+
+
+
+	}
+
+}
---
+++ operator/ConditionalOperation.java
@@ -0,0 +1,18 @@
+package com.operator;
+
+public class ConditionalOperation {
+
+	public static void main(String[] args) {
+
+//		CONDITIONAL OR TERNARY OPERATOR.
+  
+		///symbol of ternary operator...    ?:
+
+         int smallerNum = ( 144 < 20 )  ? 144 : 20;
+		
+		System.out.println("Smaller number: "+smallerNum);
+
+
+	}
+
+}
