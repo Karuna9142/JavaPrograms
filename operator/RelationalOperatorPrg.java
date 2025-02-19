bbe1949043002d7ef37e43a4b1e23d2f8a068cb2
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-02-19 23:48:49 +0530

    Add files via upload

---
+++ operator/RelationalOperatorPrg.java
@@ -0,0 +1,27 @@
+package com.operator;
+
+public class RelationalOperatorPrg {
+
+	public static void main(String[] args) {
+		
+		///Relational Operator... symbols:  <, <=, >, >=, ==
+		
+		int var1 = 100;    ////Assignment Operator...  symbol ( = )
+		int var2 = 100;
+				
+		if(var1 < var2) {
+			System.out.println("Var1 is Smaller than Var2");
+		}
+				
+		if(var1 > var2) {
+	    	System.out.println("Var1 is Greater than Var2");
+		}
+				
+		if(var1 == var2) {
+			System.out.println("Var1 is equal to Var2");
+		}
+
+
+	}
+
+}
