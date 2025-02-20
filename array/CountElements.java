4e3114b836dd39662e06944e04c2121da999611d
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-02-20 12:28:21 +0530

    Add files via upload

---
+++ array/CountElements.java
@@ -0,0 +1,18 @@
+package com.array.assignment;
+
+public class CountElements {
+
+	public static void main(String[] args) {
+		
+		// Define an array
+		int[] arr = {10, 20, 30, 40, 50, 60, 70};
+		
+		// Get the number of elements in the array
+		int count = arr.length;
+		
+		// Print the count
+		System.out.println("The number of elements in the arrayis: " + count);
+
+	}
+
+}
